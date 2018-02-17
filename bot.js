fb2987373388c0b3e3c978a9e9631ed0094c6233
51n1 <>
2018-02-17 23:56:51 +0800

    update

--- bot.js
+++ bot.js
@@ -1,59 +1,71 @@
-// 15.6 - Twitter Bot Tutorial by Daniel Shiffman
+// reference: Twitter Bot Tutorial by Daniel Shiffman
 
 var cron = require('node-cron');
 console.log('cron is starting');
-cron.schedule('0 0 0,12 * * *', () => {//second, minute, hour, day, month, weekday
+cron.schedule('0 0 0,6,12,18 * * *', () => {//second, minute, hour, day, month, weekday
 
   console.log('The p5 bot is starting');
 
   var Twit = require('twit');
-
   var config = require('./config');
   var T = new Twit(config);
   var exec = require('child_process').exec;
   var fs = require('fs');
 
-  tweetIt();
-  // setInterval(tweetIt, 1000*20);
+  fs.readdir('./p5', readFiles);
 
-  function tweetIt() {
-    var inames = ['circles', 'polka_dots', 'photoimg'];
-    var iname = inames[Math.floor(Math.random() * inames.length)];
-    // var iname = inames[2]; // for test
-    var cmd = 'p5/' + iname + '/' + iname; // on aws
-    // var cmd = 'processing-java --sketch=`pwd`/p5/' + iname + ' --run'; // on local
-
-    exec(cmd, processing);
-
-    function processing() {
-      var filename = 'p5/' + iname + '/output.png';
-      var params = {
-        encoding: 'base64'
+  function readFiles(err, pnames) {
+    for (var i = 0; i < pnames.length; i++) {
+      if (pnames[i].indexOf('.') >= 0) {
+        pnames.splice(i, 1);
       }
-      var b64 = fs.readFileSync(filename, params);
-      T.post('media/upload', {media_data: b64 }, uploaded);
+    }
 
-      function uploaded(err, data, response) {
-        var fs = require('fs');
-        var num;
-        var id = data.media_id_string;
-        fs.readFile('number.txt', { encoding: 'utf-8' }, function(err, data) {
-          num = parseInt(data);
-          num += 1;
-          fs.writeFile('number.txt', num, function(err, data){});
-          var tweet = {
-            status: 'No.' + num + ': p5 image, "' + iname + '" by p5 bot from node.js',
-            media_ids: [id]
-          }
-          T.post('statuses/update', tweet, tweeted);
-        });
+    tweetIt();
+
+    function tweetIt() {
+      var os = require('os');
+      var platform = os.platform();
+      var pname = pnames[Math.floor(Math.random() * pnames.length)];
+      // var pname = pnames[2]; // for test
+      var cmd;
+      if (platform == 'darwin') {
+        cmd = 'processing-java --sketch=`pwd`/p5/' + pname + ' --run'; // on local
+      } else {
+        cmd = 'p5/' + pname + '/' + pname; // on aws
       }
 
-      function tweeted(err, data, response) {
-        if (err) {
-          console.log("Something went wrong!");
-        } else {
-          console.log("It worked!");
+      exec(cmd, processing);
+
+      function processing() {
+        var filename = 'p5/' + pname + '/output.png';
+        var params = {
+          encoding: 'base64'
+        }
+        var b64 = fs.readFileSync(filename, params);
+        T.post('media/upload', {media_data: b64 }, uploaded);
+
+        function uploaded(err, data, response) {
+          var num;
+          var id = data.media_id_string;
+          fs.readFile('number.txt', { encoding: 'utf-8' }, function(err, data) {
+            num = parseInt(data);
+            num += 1;
+            fs.writeFile('number.txt', num, function(err, data){});
+            var tweet = {
+              status: 'No.' + num + ': "' + pname + '"',
+              media_ids: [id]
+            }
+            T.post('statuses/update', tweet, tweeted);
+          });
+        }
+
+        function tweeted(err, data, response) {
+          if (err) console.log("Something went wrong!");
+          else console.log("It worked!");
+          fs.unlink(filename, function (err) {
+            if(err) console.log("File unlink error!");
+          });
         }
       }
     }
